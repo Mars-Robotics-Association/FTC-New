90a8cbf6ddcb7004226f094fd6b06cb95d8a3114
Owen <owencanderson@icloud.com>
2020-04-17 15:06:15 -0400

    Added event-based input

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AB_ControllerInputListener.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+interface AB_ControllerInputListener
+{
+    void A1Pressed();
+    void B1Pressed();
+    void X1Pressed();
+    void Y1Pressed();
+
+    void A1Released();
+    void B1Released();
+    void X1Released();
+    void Y1Released();
+
+    void LB1Pressed();
+    void RB1Pressed();
+    void LJS1Pressed();
+    void RJS1Pressed();
+
+
+    void A2Pressed();
+    void B2Pressed();
+    void X2Pressed();
+    void Y2Pressed();
+
+    void A2Released();
+    void B2Released();
+    void X2Released();
+    void Y2Released();
+
+    void LB2Pressed();
+    void RB2Pressed();
+    void LJS2Pressed();
+    void RJS2Pressed();
+}

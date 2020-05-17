e613a0bf623ab555acbb8e95c8bb43583854d9f4
Owen <owencanderson@icloud.com>
2020-05-17 17:12:34 -0400

    Fixed Inheritance Issue and Imported Coroutines

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/ExampleAutonomousOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/ExampleAutonomousOpMode.java
@@ -3,4 +3,18 @@ package org.firstinspires.ftc.teamcode.OpModes;
 public class ExampleAutonomousOpMode extends AutonomousOpMode
 {
 
+    @Override
+    public void init() {
+        
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
 }

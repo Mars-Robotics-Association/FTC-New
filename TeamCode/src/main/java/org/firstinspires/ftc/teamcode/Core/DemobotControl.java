24239a84a5181a8fe42f30258f09f12185485690
Owen <owencanderson@icloud.com>
2020-06-29 15:20:32 -0400

    Added dependencies

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core/DemobotControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core/DemobotControl.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.Core;
 
+import org.firstinspires.ftc.teamcode.Attachments.DemoArcShooter;
+import org.firstinspires.ftc.teamcode.AutonomousFunctions.DemoChassisAutoFuncs;
+import org.firstinspires.ftc.teamcode.AutonomousFunctions.DemoShooterAutoFuncs;
 import org.firstinspires.ftc.teamcode.Odometry.DemoBotOdometry;
 
 public class DemobotControl
@@ -7,4 +10,18 @@ public class DemobotControl
     //Dependencies
     private DemoBotOdometry Odometry;
     private IMU Imu;
+    private DemoArcShooter Shooter;
+    private DemoChassisAutoFuncs ChassisAutoFuncs;
+    private DemoShooterAutoFuncs ShooterAutoFuncs;
+
+    //Variables
+
+
+    //Initializer
+    public DemobotControl()
+    {
+
+    }
+
+    //Methods
 }

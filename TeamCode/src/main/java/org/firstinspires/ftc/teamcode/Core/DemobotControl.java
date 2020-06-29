e571a330b5d73d09e2374fca2581d4c7abc3062c
Owen <owencanderson@icloud.com>
2020-06-29 15:16:17 -0400

    Added a few dependencies

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core/DemobotControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core/DemobotControl.java
@@ -1,6 +1,10 @@
 package org.firstinspires.ftc.teamcode.Core;
 
+import org.firstinspires.ftc.teamcode.Odometry.DemoBotOdometry;
+
 public class DemobotControl
 {
-    
+    //Dependencies
+    private DemoBotOdometry Odometry;
+    private IMU Imu;
 }

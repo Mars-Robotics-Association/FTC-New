3dfac0f7083c65d2a258b9f77c89ea3cfa13ac73
Owen <owencanderson@icloud.com>
2020-09-17 22:42:40 -0400

    Added move relative to vumark for shooting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Navigation/OrionNavigator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Navigation/OrionNavigator.java
@@ -10,6 +10,7 @@ package org.firstinspires.ftc.teamcode.Navigation;
 //Roadrunner Package
 //Orion Package
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.teamcode.Navigation.Roadrunner.RoadrunnerControl;
@@ -36,12 +37,16 @@ public class OrionNavigator
     public void MoveToVumark(int vumarkIndex, double xOffset, double yOffset, double headingOffset, double xThreshold, double yThreshold){
         //Move to an offset relative to a vumark and face it- useful for shooting
 
-        //Find vumark dist
+        //Find vumark pose
         double[] vumarkDist = v.GetData(vumarkIndex);
-        //Find offset from vumark
-        double offsetX = vumarkDist[0] - xOffset;
-        double offsetY = vumarkDist[1] - yOffset;
-        double offsetH = vumarkDist[2] - headingOffset;
+        //Find current robot pose
+        Pose2d robotPose = rr.GetCurrentPose();
+
+        //Find offset from vumark in world space //TODO: make dynamic with start pos of robot
+        double offsetX = xOffset - vumarkDist[0] + robotPose.getX();
+        double offsetY = yOffset - vumarkDist[1] + robotPose.getY();
+        double offsetH = headingOffset - vumarkDist[2] + robotPose.getHeading();
+
         //Move spline
         MoveSpline(offsetX, offsetY, 0);
         //Turn to face target

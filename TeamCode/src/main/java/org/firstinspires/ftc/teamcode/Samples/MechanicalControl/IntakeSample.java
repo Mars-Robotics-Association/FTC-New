ce2ad8c8b387facc89e391f1dd7466ea10af6c5f
Owen <owencanderson@icloud.com>
2021-03-16 21:11:19 -0400

    Sample code for Robot Class Guide

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samples/MechanicalControl/IntakeSample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samples/MechanicalControl/IntakeSample.java
@@ -4,27 +4,26 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.MechanicalControl.MotorizedIntake;
 
-/*Sample class for MechanicalControl.Intake
-Intake goes forwards when right trigger is down and reverses when left trigger is down. Stops when nothing is down.*/
-@TeleOp(name = "IntakeSample", group = "Samples")
-@Disabled
-public class IntakeSample extends OpMode
+public class IntakeSample
 {
-    MotorizedIntake intake;
+    private Servo servoR;
+    private Servo servoL;
 
-    @Override
-    public void init() {
-        intake = new MotorizedIntake();
-        intake.Init(new DcMotor[]{hardwareMap.dcMotor.get("motor1")}, new double[]{1});
+    public IntakeSample(Servo setServoR, Servo setServoL){
+        servoR = setServoR;
+        servoL = setServoL;
     }
 
-    @Override
-    public void loop() {
-        if(gamepad1.right_trigger > 0.1) intake.SetIntakePower(1);
-        else if(gamepad1.left_trigger > 0.1) intake.SetIntakePower(-1);
-        else intake.SetIntakePower(0);
+    public void IntakeOn(){
+        servoR.setPosition(1);
+        servoL.setPosition(0);
+    }
+    public void IntakeOff(){
+        servoR.setPosition(0.5);
+        servoL.setPosition(0.5);
     }
 }
